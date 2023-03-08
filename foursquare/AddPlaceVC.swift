14dbf7fce14268da31546b76ccaeac6505129543
Lodkahs <omalojiu@gmail.com>
2023-03-08 20:04:14 +0200

    adding AddPlaceVC

---
+++ foursquare/AddPlaceVC.swift
@@ -0,0 +1,34 @@
+//
+//  AddPlaceVC.swift
+//  foursquare
+//
+//  Created by Andrew  on 3/8/23.
+//
+
+import UIKit
+
+class AddPlaceVC: UIViewController {
+    
+    
+    @IBOutlet weak var placeNameText: UITextField!
+    
+    @IBOutlet weak var placeTypeText: UITextField!
+    
+    @IBOutlet weak var commentText: UITextField!
+    
+    @IBOutlet weak var placeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        
+    }
+    
+    
+
+}
