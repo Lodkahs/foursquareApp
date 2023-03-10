76df27d41db8a553760d92bc76c5432e4ce56866
Lodkahs <omalojiu@gmail.com>
2023-03-10 22:28:03 +0200

    get details about cell from Parse

--- foursquare/DetailsVC.swift
+++ foursquare/DetailsVC.swift
@@ -23,12 +23,17 @@ class DetailsVC: UIViewController {
     @IBOutlet weak var detailsMapView: MKMapView!
     
     var chosenPlaceID = ""
-    
-    
+    var chosenLatitude = Double()
+    var chosenLongitude = Double()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        getDataFromParse()
+        
+    }
+    
+    func getDataFromParse() {
         let query = PFQuery(className: "Places")
         query.whereKey("objectId", equalTo: chosenPlaceID)
         query.findObjectsInBackground { objects, error in
@@ -36,7 +41,43 @@ class DetailsVC: UIViewController {
             if error != nil {
                 //
             } else {
-                print(objects)
+                if objects != nil {
+                    if objects!.count > 0 {
+                        let chosenPlaceObject = objects![0]
+                        
+                        if let placeName = chosenPlaceObject.object(forKey: "name") as? String {
+                            self.detailsPlacenameLabel.text = placeName
+                        }
+                        if let placeType = chosenPlaceObject.object(forKey: "type") as? String {
+                            self.detailsTypeLabel.text = placeType
+                        }
+                        if let placeComment = chosenPlaceObject.object(forKey: "comment") as? String {
+                            self.detailsCommentLabel.text = placeComment
+                        }
+                        if let placeLatitude = chosenPlaceObject.object(forKey: "latitude") as? String {
+                            if let placeLatitudeDouble = Double(placeLatitude) {
+                                self.chosenLatitude = placeLatitudeDouble
+                            }
+                        }
+                        if let placeLongitude = chosenPlaceObject.object(forKey: "longitude") as? String {
+                            if let placeLongitudeDouble = Double(placeLongitude) {
+                                self.chosenLongitude = placeLongitudeDouble
+                            }
+                        }
+                        if let imageData = chosenPlaceObject.object(forKey: "image") as? PFFileObject {
+                            imageData.getDataInBackground { data, error in
+                                if error == nil {
+                                    if data != nil {
+                                        self.detailsImageView.image = UIImage(data: data!)
+                                    }
+                                    
+                                }
+                            }
+                        }
+                    }
+                    
+                    
+                }
             }
         }
     }
